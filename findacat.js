669fbffdf96782cc070c0b24493834244e3d6e33
leknaat <wishfire@gmail.com>
2017-02-07 16:45:02 +0000

    Removed link

--- findacat.js
+++ findacat.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	"http://www.elginpk.com/worsley1011_1/gray/cat%204.jpg",
 	"https://s-media-cache-ak0.pinimg.com/originals/ed/8e/3a/ed8e3a1b888377a9751d86ebd33a4c47.jpg"]
 
-   $('button').on('click', function(){
+   $('.button').on('click', function(){
    	$(images[Math.floor(Math.random()*images.length)]).click()
   });
 });
