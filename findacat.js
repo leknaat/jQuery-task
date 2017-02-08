4d0f5bc5252dd84083fde6e3c373aa58deed8a35
leknaat <wishfire@gmail.com>
2017-02-08 11:32:49 +0000

    finally switches images!

--- findacat.js
+++ findacat.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 	"http://www.cutestpaw.com/wp-content/uploads/2011/11/To-infinity-and-beyond.jpeg",
 	"http://powellong.com/data/wallpapers/199/WDF_2369253.jpg",
 	"http://www.elginpk.com/worsley1011_1/gray/cat%204.jpg",
-	"https://s-media-cache-ak0.pinimg.com/originals/ed/8e/3a/ed8e3a1b888377a9751d86ebd33a4c47.jpg"]
+	"https://s-media-cache-ak0.pinimg.com/originals/ed/8e/3a/ed8e3a1b888377a9751d86ebd33a4c47.jpg"];
 
    $('.button').on('click', function(){
-   	$(images[Math.floor(Math.random()*images.length)]).click()
+   	 var rnd = Math.floor(Math.random() * images.length);
+     var url = images[rnd];
+   	$( ".cats" ).attr({src: url});
   });
 });
-
-  
